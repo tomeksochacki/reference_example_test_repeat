fc9aec67899eaee4f363d66f9b4c4cdb318d36f3
tomek <tomeksochacki@wp.pl>
2020-06-04 15:10:19 +0200

    Update

--- src/com/testexampletestreference/Main.java
+++ src/com/testexampletestreference/Main.java
@@ -3,6 +3,18 @@ package com.testexampletestreference;
 public class Main {
     public static void main(String[] args) {
         int a = 20;
+        double b = 40;
+        int c = 20;
+        String name = new String("Tomek");
+        String name3 = new String ("Tomek");
+
+        if (name3.equals(name)){
+            System.out.println("These names are equals");
+        }
+
+
+
+
 
         Test test1 = new Test();
         a = test1.changeValue(a);
@@ -13,6 +25,11 @@ public class Main {
         String name1 = "Tomek";
         String name2 = test1.changeName(name1);
         System.out.println(name2);
+        b = test1.changeValueD(b);
+        System.out.println(b);
+        c = test1.changeValue(c);
+        System.out.println(c);
+
     }
 }
 
@@ -31,10 +48,19 @@ class Test{
         name = name + " Sochacki";
         return name;
     }
+
+    double changeValueD(double chVD){
+        chVD = chVD + 4;
+        return chVD;
+    }
 }
 
 class Foo{
     int y = 20;
+}
+
+class Example{
+    double v = 3.4;
 }
 
 
